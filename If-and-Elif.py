2ace5f239b21c1e99524115bbc3ad930e31fddc1
arjuna1011 <jgboatright@att.net>
2023-04-17 16:53:48 -0400

    finished with if and elif for section 4

--- If-and-Elif.py
+++ If-and-Elif.py
@@ -22,19 +22,34 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher.")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it!")
-    else:
-        print("Sorry, you were incorrect.")
-elif guess > answer:
-    print("Please guess lower.")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it!")
-    else:
-        print("Sorry, you were incorrect.")
+# if guess < answer: # If the guess is less than the answer
+#     print("Please guess higher.") 
+#     guess = int(input())
+#     if guess == answer: 
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry, you were incorrect.")
+# elif guess > answer:   # If the guess is greater than the answer
+#     print("Please guess lower.")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry, you were incorrect.")
+# else:   # If the answer is correct
+#     print("Well done, you guessed it!")
+
+# Or 
+
+if guess == answer:
+    print("You got it first time!")
 else:
-    print("Well done, you guessed it!")
+    if guess < answer:
+        print("Please guess higher.")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("You got it correct!")
+    else:
+        print("Sorry, you are incorrect!")
