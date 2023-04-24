4478a3b433e93153dd51eea2460488f92f035ff9
arjuna1011 <jgboatright@att.net>
2023-04-23 22:34:06 -0400

    Jukebox

--- Tuples.py
+++ Tuples.py
@@ -59,7 +59,7 @@ albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
           ("Bad Company", "Bad Company", 1974),
           ("Nightflight", "Budgie", 1981),
           ("More Mayhem", "Emilda May", 2011),
-          ("Ride the Lightning", "Metallica", 1984),
+          ("Ride The Lightning", "Metallica", 1984)
           ]
 print(len(albums)) # If we didn't put each entry in the parentheses, we would get 15 instead of 5.
 
