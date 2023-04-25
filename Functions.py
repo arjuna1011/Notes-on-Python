0df523013486c8cbb69851718029dad7135ac8c9
arjuna1011 <jgboatright@att.net>
2023-04-24 23:44:54 -0400

    Functions intro

---
+++ Functions.py
@@ -0,0 +1,10 @@
+# To start a function, you must use the keyword "def".
+# Then you will have the name of the function. "multiply" in this case.
+# And then you will have parameters, you must still include the () at the end of the function name even if there are no parameters being passed through.
+
+def multiply():
+    result = 10.5 * 4
+    return result
+
+answer = multiply()
+print(answer)
